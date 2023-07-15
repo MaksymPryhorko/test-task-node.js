35bc6f9e326e9bff3ea6b212ec04eab8bf0bcc82
MaksymPryhorko <maxik2007@ukr.net>
2023-07-15 11:52:53 +0300

    bot.command /name/age/email

--- index.js
+++ index.js
@@ -1,34 +1,83 @@
-const express = require("express");
-const cors = require("cors");
-const usersRouter = require("./routes/api/users");
-const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const { Telegraf } = require("telegraf");
+const { message } = require("telegraf/filters");
+require("dotenv").config();
 
-dotenv.config();
-const { DB_HOST, PORT = 3000 } = process.env;
+const bot = new Telegraf(process.env.BOT_TOKEN);
+console.log("Start Bot");
 
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log("Server running.");
-    });
-    console.log("Database connection successful");
-  })
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
+const newUser = {
+  name: null,
+  age: null,
+  email: null,
+};
 
-app.use(cors());
-app.use(express.json());
+const resetNewUser = function () {
+  newUser.name = null;
+  newUser.age = null;
+  newUser.email = null;
+};
 
-app.use("/api", usersRouter);
+const changeDataNewUser = function (key, value) {
+  newUser[key] = value;
+};
 
-app.use((req, res) => res.status(404).json({ message: "Not found" }));
+const validateEmail = function (email) {
+  const re =
+    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+  return re.test(String(email).toLowerCase());
+};
 
-app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error." } = err;
-  res.status(status).json({ message });
+bot.start((ctx) => {
+  ctx.reply(
+    `Welcome, ${
+      ctx.from.first_name ? ctx.from.first_name : "Unknown"
+    }. Команды для ввода нового пользователя: /name, /age, /email`
+  );
+  resetNewUser();
 });
+
+bot.command("name", (ctx) => {
+  ctx.reply("Веддите имя нового пользователя!");
+  bot.on("text", (ctx) => {
+    if (newUser.name === null) {
+      ctx.reply(
+        `Имя нового пользователя "${ctx.message.text}". Теперь введите возраст /age и email /email.`
+      );
+      changeDataNewUser("name", ctx.message.text);
+    }
+  });
+});
+
+bot.command("age", (ctx) => {
+  ctx.reply("Веддите возраст нового пользователя!");
+  bot.on("text", (ctx) => {
+    const age = Number.parseInt(ctx.message.text);
+    if (age > 0) {
+      ctx.reply(`Успешно. Возраст нового пользователя ${age} лет. Теперь введите email /email.`);
+      changeDataNewUser("age", age);
+    } else
+      ctx.reply(
+        "Ошибка. Возраст должен быть больше нуля лет. Попробуйте еще раз, через команду /age"
+      );
+  });
+});
+
+bot.command("email", (ctx) => {
+  ctx.reply("Веддите email нового пользователя!");
+  bot.on("text", (ctx) => {
+    const email = ctx.message.text;
+    if (validateEmail(email)) {
+      changeDataNewUser("email", email);
+      ctx.reply(`Успешно. ${email} - email нового пользователя.`);
+    } else {
+      ctx.reply(`Ошибка. ${email} - введен не корректно. Поробуйте еще раз, через команду /email`);
+    }
+  });
+});
+
+bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+bot.launch();
+
+// Enable graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
