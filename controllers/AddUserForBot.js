395af5d2fe007b57c6b51c8ff41d698a3e790e83
MaksymPryhorko <maxik2007@ukr.net>
2023-07-16 22:11:06 +0300

    delete file AddUserForBot

--- controllers/AddUserForBot.js
+++
@@ -1,8 +0,0 @@
-const { User } = require("../models/user");
-
-const addUserForBot = async (newUser) => {
-  const addUser = await User.create(newUser);
-  return addUser;
-};
-
-module.exports = addUserForBot;
